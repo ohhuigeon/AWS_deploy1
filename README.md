4a4705d1599ffca161e937a1e75f9fb18ecd9a13
ohhuigeon <gmlrjs1002@naver.com>
2023-12-13 19:42:27 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# AWS 강의 첫번째 배포
+
+### Springboot 3.2.0, JDK17
+- devtools
+- springweb
+- lombok
+
+### 배포방법
+- 로컬에서 github 업로드
+- EC2 에서 github 다운로드
+- 프로젝트 테스트
+- 프로젝트 빌드
+- nohub 으로 백그라운드 실행
+- 오류 로그 남기기 (표준 입출력 리다이렉션)
+- 서버가 종료되면 cron으로 자동 재시작
---
+++ src/main/java/com/lec/spring/AwsDeploy1Application.java
@@ -0,0 +1,14 @@
+package com.lec.spring;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsDeploy1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsDeploy1Application.class, args);
+	}
+
+}
